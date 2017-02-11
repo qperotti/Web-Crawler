e551352f949cc9dd35f904793397fd890668afb3
Joaquim <qperotti@gmail.com>
2017-02-11 17:44:37 -0500

    ss

--- spider.py
+++ spider.py
@@ -7,14 +7,24 @@ from general import *
 
 class Spider:
 
+	## master change
+	###
+
 	# Class variables (shared among all instances)
 	project_name = ''
 	base_url = ''
-	domain_name = ''
+	domain_name_name = ''
 	queue_file = ''
 	crawled_file = ''
 	queue = set()
 	crawled = set()
+	####################
+
+
+
+	#### this is an update
+	###snoasd
+
 
 	def __init__(self, project_name, base_url, domain_name):
 		Spider.project_name = project_name
