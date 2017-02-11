0146ba9d330ac380d05e8647400574bc48a7f34e
Joaquim <qperotti@gmail.com>
2017-02-11 17:43:02 -0500

    update

--- spider.py
+++ spider.py
@@ -7,24 +7,14 @@ from general import *
 
 class Spider:
 
-	## master change
-	###
-
 	# Class variables (shared among all instances)
 	project_name = ''
 	base_url = ''
-	domain_name_name = ''
+	domain_name = ''
 	queue_file = ''
 	crawled_file = ''
 	queue = set()
 	crawled = set()
-	####################
-
-
-
-	#### this is an update
-	###snoasd
-
 
 	def __init__(self, project_name, base_url, domain_name):
 		Spider.project_name = project_name
